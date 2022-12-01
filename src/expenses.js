9fb5dc0f7925e8e569f5273a835102f419ba5029
Maisha Mir <>
2022-11-30 23:23:18 -0800

    Initial finished project

---
+++ src/expenses.js
@@ -0,0 +1,49 @@
+import expenseData from '../data.json' assert{type: 'json'};
+var chart = document.getElementById("expenses")
+
+var days = []
+var amount = []
+
+for (let i in expenseData) {
+    days.push(expenseData[i].day)
+    amount.push(expenseData[i].amount)
+}
+
+new Chart('expenses', {
+    type: 'bar',
+    data: {
+        labels: days,
+        datasets: [{
+            backgroundColor: 'hsl(10, 79%, 65%)',
+            hoverBackgroundColor: "hsl(186, 34%, 60%)",
+            data: amount,
+            borderRadius: 5,
+            borderSkipped: false,
+        }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                display: false
+            },
+        },
+        scales: {
+            y: {
+                grid: {
+                    display: false,
+                },
+                display: false,
+            },
+            x: {
+                grid: {
+                    display: false,
+                },
+                border: {
+                    display: false,
+                }
+            },
+            
+        }
+    }
+})
+
